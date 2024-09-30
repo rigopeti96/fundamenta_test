5b8b0244d22e5a915b023cab76f6fead496b874b
Péter Attila Rigó <61515182+rigopeti96@users.noreply.github.com>
2024-09-30 13:49:15 +0200

    Validation tests done

--- src/test/java/ValidatorTest.java
+++ src/test/java/ValidatorTest.java
@@ -1,6 +1,5 @@
 import entity.Product;
-import exception.ProductTypeIsEmptyException;
-import exception.ProductTypeIsInvalidException;
+import exception.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -45,7 +44,53 @@ public class ValidatorTest {
     }
 
     @Nested
+    @DisplayName("Business associate name validation tests")
     class BusinessValidationTest{
+        @Test
+        @DisplayName("Success")
+        public void success(){
+            assertTrue(validator.validateBusinessAssociate("Ü1"));
+            assertTrue(validator.validateBusinessAssociate("Ü2"));
+            assertTrue(validator.validateBusinessAssociate("Ü3"));
+        }
 
+        @Test
+        @DisplayName("Throws empty exception")
+        public void throwsEmptyException(){
+            assertThrows(BusinessAssociateIsEmptyException.class, () -> validator.validateBusinessAssociate(""));
+        }
+
+        @Test
+        @DisplayName("Throws invalid exception")
+        public void throwsInvalidException(){
+            assertThrows(BusinessAssociateIsInvalidException.class, () -> validator.validateBusinessAssociate("Ü4"));
+        }
+    }
+
+    @Nested
+    @DisplayName("Product prize validation tests")
+    class ProductPrizeValidationTest{
+        @Test
+        @DisplayName("Success")
+        public void success(){
+            assertTrue(validator.validateProductPrize(0));
+            assertTrue(validator.validateProductPrize(10000));
+        }
+
+        @Test
+        @DisplayName("Throws empty exception")
+        public void throwsEmptyException(){
+            Integer productPrizeNull = null;
+
+            assertThrows(ProductPrizeIsEmptyException.class, () -> validator.validateProductPrize(productPrizeNull));
+        }
+
+        @Test
+        @DisplayName("Throws invalid exception")
+        public void throwsInvalidException(){
+            Integer productLessThanZero = -1;
+
+            assertThrows(ProductPrizeIsInvalidException.class, () -> validator.validateProductPrize(productLessThanZero));
+        }
     }
 }
